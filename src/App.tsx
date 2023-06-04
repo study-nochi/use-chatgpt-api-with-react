62a0646386e23f70b667c2a51d388e1737a5e0da
ainochi-kor <kthrkdals@gmail.com>
2023-06-04 17:08:24 +0900

    @chatscope/chat-ui-kit-react 이거 괜찮네

---
+++ src/App.tsx
@@ -0,0 +1,115 @@
+import { useState } from "react";
+import "./App.css";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+  MessageModel,
+} from "@chatscope/chat-ui-kit-react";
+import { CHAT_GPT_API_KEY } from "./config";
+
+function App() {
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessage] = useState<MessageModel[]>([
+    {
+      message: "Hello, I am ChatGPT",
+      sender: "ChatGPT",
+      direction: "incoming",
+      position: "first",
+    },
+  ]);
+
+  const handleSend = async (message: string) => {
+    const newMessage: MessageModel = {
+      message,
+      sender: "user",
+      direction: "outgoing",
+      position: "normal",
+    };
+
+    const newMessages = [...messages, newMessage];
+    setMessage(newMessages);
+
+    setTyping(true);
+
+    processMessageToChatGPT(newMessages);
+  };
+
+  async function processMessageToChatGPT(chatMessages: MessageModel[]) {
+    // chatMessages { sender: "user" or "ChatGPT", message: "The message content here"}
+    // apiMesssages { role: "user" or "assistant", content: "The message content here"}
+
+    const apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role, content: messageObject.message };
+    });
+
+    const systemMessage = {
+      role: "system",
+      content: "Speack like a Korean",
+    };
+
+    const apiRequestBody = {
+      model: "gpt-3.5-turbo",
+      messages: [systemMessage, ...apiMessages],
+    };
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + CHAT_GPT_API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setMessage([
+          ...chatMessages,
+          {
+            message: data.choices[0].message.content,
+            sender: "ChatGPT",
+            direction: "incoming",
+            position: "normal",
+          },
+        ]);
+        setTyping(false);
+      });
+  }
+
+  return (
+    <div className="App">
+      <div style={{ position: "relative", height: "800px", width: "700px" }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+              scrollBehavior="smooth"
+              typingIndicator={
+                typing ? <TypingIndicator content="ChatGPT is typing" /> : null
+              }
+            >
+              {messages?.map((message, i) => {
+                return <Message key={i} model={message} />;
+              })}
+            </MessageList>
+            <MessageInput placeholder="Type message here" onSend={handleSend} />
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config/index.ts
@@ -0,0 +1,1 @@
+export const CHAT_GPT_API_KEY = import.meta.env.VITE_CHAT_GPT_API_KEY;
